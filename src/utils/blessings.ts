cd17f73c754bb585e10605d93384e6d42c264442
danyelvarejao <danyelvarejao@gmail.com>
2023-03-23 14:26:40 -0300

    fix: twist of fate blessing price

--- src/utils/blessings.ts
+++ src/utils/blessings.ts
@@ -1,27 +1,34 @@
 import { Blessing } from 'src/types';
 
 const getBlessingCost = (blessing: Blessing, level: number): number => {
-  if (level <= 30) {
-    if (blessing.type === 'enhanced') {
-      return 2600;
+  if (blessing.type === 'regular') {
+    if (level <= 30) {
+      return 2000;
+    } else if (level <= 120) {
+      return 200 * (level - 20);
+    } else {
+      return 20000 + 75 * (level - 120);
     }
-    return 2000;
-  }
-
-  if (level <= 120) {
-    if (blessing.type === 'enhanced') {
+  } else if (blessing.type === 'enhanced') {
+    if (level <= 30) {
+      return 2600;
+    } else if (level <= 120) {
       return 260 * (level - 20);
     } else {
+      return 26000 + 100 * (level - 120);
+    }
+  } else if (blessing.type === 'pvp') {
+    if (level <= 30) {
+      return 2000;
+    } else if (level <= 120) {
       return 200 * (level - 20);
+    } else if (level <= 270) {
+      return 200 * (level - 20);
+    } else {
+      return 50000;
     }
   }
-
-  if (blessing.type === 'enhanced') {
-    return 26000 + 100 * (level - 120);
-  } else if (blessing.type === 'regular') {
-    return 20000 + 75 * (level - 120);
-  }
-  return 20000;
+  return 0;
 };
 
 export { getBlessingCost };
